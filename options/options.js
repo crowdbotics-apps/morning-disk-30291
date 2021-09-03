27da9fadbcd7e7dd21695207b9e73b31b199ce4f
Crowdbotics <team@crowdbotics.com>
2021-09-03 04:08:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "morning_disk_30291Identifier",
+  url: "https://morning_disk_30291Identifier.botics.co",
+  api: "https://morning_disk_30291Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
